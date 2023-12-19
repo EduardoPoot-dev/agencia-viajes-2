ed55f025a5221c1ee7fa7128f8ebf0ec4aad7635
EduardoPoot-dev <131505277+EduardoPoot-dev@users.noreply.github.com>
2023-12-19 00:35:34 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,16 +4,12 @@ dotenv.config();
 
 //configuramos nuestra base de datos
 const db = new Sequelize(process.env.DATABASE_URL, {
-    define: {
-        timestramps: false
-    },
     pool: {
         max: 2,
-        min: 0,
+        min: 1,
         acquire: 30000,
         idle: 10000
-    },
-    operatorAliases: false,
-})
+    }
+}
 
 export default db;
