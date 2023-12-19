2f5b31178155ceaed0c1933ed268dc193f2bb488
EduardoPoot-dev <131505277+EduardoPoot-dev@users.noreply.github.com>
2023-12-19 00:33:06 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const db = new Sequelize(process.env.DATABASE_URL, {
         timestramps: false
     },
     pool: {
-        max: 3,
+        max: 2,
         min: 0,
         acquire: 30000,
         idle: 10000
