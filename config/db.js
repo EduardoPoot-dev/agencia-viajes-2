c7fb5dcb346b12e55c46268e10152f0e9582d959
Eduardo Poot <eduardopoot_@outlook.com>
2023-12-19 00:20:00 -0600

    agencia viajes 2

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const db = new Sequelize(process.env.DATABASE_URL, {
         timestramps: false
     },
     pool: {
-        max: 5,
+        max: 15,
         min: 0,
         acquire: 30000,
         idle: 10000
