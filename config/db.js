ddbb41284841829da77519d19dc249ee1e493ac8
EduardoPoot-dev <131505277+EduardoPoot-dev@users.noreply.github.com>
2023-12-19 12:16:34 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -16,23 +16,5 @@ const db = new Sequelize(process.env.DATABASE_URL, {
     operatorAliases: false,
 })
 
-// Realiza operaciones con la base de datos
-async function performDatabaseOperations() {
-    try {
-      // Tu lógica para realizar operaciones en la base de datos aquí
-      // Por ejemplo, consultar datos
-      const users = await sequelize.query('SELECT * FROM users', { type: Sequelize.QueryTypes.SELECT });
-      console.log(users);
-  
-      // Cierra la conexión después de usarla
-      await sequelize.close();
-      console.log('Conexión cerrada correctamente');
-    } catch (error) {
-      console.error('Error al realizar operaciones en la base de datos:', error);
-    }
-  }
-  
-  // Llama a la función para realizar operaciones en la base de datos
-  performDatabaseOperations();
 
 export default db;
